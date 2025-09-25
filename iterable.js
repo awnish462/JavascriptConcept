4975a1383d58ec073ddeb38bf1ff08f86800fe43
Awnish462 <awnishsinha462@gmail.com>
2025-09-25 14:02:58 +0530

    Iterable and Iterator Concept

---
+++ iterable.js
@@ -0,0 +1,32 @@
+// #Rule
+
+const iterable = {
+  [Symbol.iterator]() {
+    return {
+      next() {
+        return { value: "item", done: false };
+      }
+    };
+  }
+};
+
+const iterableObj = {
+  data: [10, 20, 30],
+  [Symbol.iterator]() {
+    let index = 0;
+    return {
+      next: () => {
+        if (index < this.data.length) {
+          return { value: this.data[index++], done: false };
+        }
+        return { done: true };
+      },
+    };
+  },
+};
+
+console.log(iterableObj)
+
+for (const item of iterableObj){
+    console.log(item)
+}
